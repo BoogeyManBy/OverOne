e76b894847a92cddf6eacf8fc181b47ed466533f
DedHabar <ubloodok@gmail.com>
2021-04-02 21:01:17 +0300

    For - 10 циклов и 100 циклов.

---
+++ src/lesson1/homework/For10.java
@@ -0,0 +1,9 @@
+package lesson1.homework;
+
+public class For10 {
+    public static void main(String[] args) {
+        for (int a = 1; a < 11; a++) {
+            System.out.println("Цикл номер: " + a);
+        }
+    }
+}
---
+++ src/lesson1/homework/For100.java
@@ -0,0 +1,10 @@
+package lesson1.homework;
+
+public class For100 {
+    public static void main(String[] args) {
+        for (int b = 1; b < 101; b++) {
+            System.out.println("Цикл номер: " + b);
+        }
+    }
+
+}
