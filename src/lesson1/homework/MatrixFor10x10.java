9bb48ab2342467f528000291221bc7e361f60cba
DedHabar <ubloodok@gmail.com>
2021-04-04 22:03:06 +0300

    Таблица умножения 10х10 через цикл for

---
+++ src/lesson1/homework/MatrixFor10x10.java
@@ -0,0 +1,45 @@
+package lesson1.homework;
+
+public class MatrixFor10x10 {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 10; a++) {
+            System.out.print(a + "| ");
+        }
+        System.out.println(" ");
+        for (int b = 2; b <= 20; b += 2) {
+            System.out.print(b + "| ");
+        }
+        System.out.println(" ");
+        for (int c = 3; c <= 30; c += 3) {
+            System.out.print(c + "| ");
+        }
+        System.out.println(" ");
+        for (int d = 4; d <= 40; d += 4) {
+            System.out.print(d + "| ");
+        }
+        System.out.println(" ");
+        for (int e = 5; e <= 50; e += 5) {
+            System.out.print(e + "| ");
+        }
+        System.out.println(" ");
+        for (int f = 6; f <= 60; f += 6) {
+            System.out.print(f + "| ");
+        }
+        System.out.println(" ");
+        for (int g = 7; g <= 70; g += 7) {
+            System.out.print(g + "| ");
+        }
+        System.out.println(" ");
+        for (int h = 8; h <= 80; h += 8) {
+            System.out.print(h + "| ");
+        }
+        System.out.println(" ");
+        for (int i = 9; i <= 90; i += 9) {
+            System.out.print(i + "| ");
+        }
+        System.out.println(" ");
+        for (int j = 10; j <= 100; j += 10) {
+            System.out.print(j + "| ");
+        }
+    }
+}
