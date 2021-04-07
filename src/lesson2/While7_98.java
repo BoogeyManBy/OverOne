ecec1d692e5b4db744812e566b8a2d79d736c09f
DedHabar <ubloodok@gmail.com>
2021-04-07 21:55:14 +0300

    Последовательность от 7 до 98 кратная 7, через while.

---
+++ src/lesson2/While7_98.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+public class While7_98 {
+    public static void main(String[] args) {
+        int a = 7;
+        while (a <= 98) {
+            System.out.print(a + " ");
+            a += 7;
+        }
+    }
+}
