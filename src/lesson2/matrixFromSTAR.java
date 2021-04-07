31e5c48cbfa3e9f0085ef504e748fb66762861a6
DedHabar <ubloodok@gmail.com>
2021-04-07 23:05:24 +0300

    Задача на звездочки с вводом данных через for и Scanner.

---
+++ src/lesson2/matrixFromSTAR.java
@@ -0,0 +1,22 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class matrixFromSTAR {
+    public static void main(String[] args) {
+        Scanner vvodA = new Scanner(System.in);
+        int ab;
+        System.out.print("Введите значение а: ");
+        ab = vvodA.nextInt();
+        Scanner vvodB = new Scanner(System.in);
+        int ba;
+        System.out.print("Введите значение b: ");
+        ba = vvodB.nextInt();
+        for (int a = 0; a < ab; a++) {
+            for (int b = 0; b < ba; b++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
