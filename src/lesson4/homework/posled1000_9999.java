dd481c8d9f1b9a8bb3bc9733b1412e4780f0bda4
DedHabar <ubloodok@gmail.com>
2021-04-21 19:03:13 +0300

    последовательность от 1000 до 9999 с шагом 3

---
+++ src/lesson4/homework/posled1000_9999.java
@@ -0,0 +1,12 @@
+package lesson4.homework;
+
+public class posled1000_9999 {
+    public static void main(String[] args) {
+        for (int i = 1000; i < 9999; i+=3) {
+            if (i>9999) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
