53b5010ed2a7adf2bf1e833ffb8c869fd7259f1c
DedHabar <ubloodok@gmail.com>
2021-04-21 19:27:45 +0300

    Вывести неотрицательные элементы последовательности 90 85 80 ...

---
+++ src/lesson4/homework/NotNegative.java
@@ -0,0 +1,14 @@
+package lesson4.homework;
+
+/**
+ * Создайте программу, выводящую на экран
+ * все неотрицательные элементы последовательности
+ * 90 85 80 75 70 65 60 ….
+ */
+public class NotNegative {
+    public static void main(String[] args) {
+        for (int i = 90; i >= 0; i -= 5) {
+            System.out.println(i);
+        }
+    }
+}
