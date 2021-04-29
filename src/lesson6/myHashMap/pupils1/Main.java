4ce0a5138735e63243f953fbc32ab426934d3964
DedHabar <ubloodok@gmail.com>
2021-04-29 18:46:55 +0300

    ArrayList - Создать список оценок учеников с помощью ArrayList, заполнить случайными оценками. Удалить неудовлетворительные оценки из списка.

--- src/lesson6/myHashMap/pupils1/Main.java
+++ src/lesson6/myHashMap/pupils1/Main.java
@@ -9,18 +9,15 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
+        int neud = 3;
+        int otl = 10;
         ArrayList<Integer> mark = new ArrayList<>();
-        mark.add(3);
-        mark.add(6);
-        mark.add(8);
-        mark.add(2);
-        mark.add(5);
-        mark.add(1);
-        mark.add(10);
+        for (int i = 0; i < otl; i++) {
+            Integer b = (int)(Math.random()*10+1);
+            mark.add(i, b);
+        }
         System.out.println(mark);
-        mark.remove(0);
-        mark.remove(2);
-        mark.remove(3);
+        mark.removeIf(integer -> integer <= neud);
         System.out.println(mark);
     }
 }
---
+++ src/lesson6/myHashMap/pupils2/Main.java
@@ -0,0 +1,11 @@
+package lesson6.myHashMap.pupils2;
+
+/**
+ * Создать коллекцию, наполнить ее случайными числами. Удалить
+ * повторяющиеся числа.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
